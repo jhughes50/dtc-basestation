1c3fa29350fb9bb3100a66ddcb1a9d6b824a4faf
marcelhussing <marcel.hussing@gmail.com>
2024-09-26 15:07:57 -0400

    i was stupid

--- ws/src/dtc_inference/src/scorecard_sender_node.py
+++ ws/src/dtc_inference/src/scorecard_sender_node.py
@@ -665,14 +665,14 @@ class ScorecardSenderNode:
                 new_row = pd.DataFrame(
                     {
                         "casualty_id": [casualty_id],
-                        "trauma_head": [agg_res["trauma_head"]],
-                        "trauma_torso": [agg_res["trauma_torso"]],
-                        "trauma_lower_ext": [agg_res["trauma_lower_ext"]],
-                        "trauma_upper_ext": [agg_res["trauma_upper_ext"]],
-                        "alertness_ocular": [agg_res["alertness_ocular"]],
-                        "severe_hemorrhage": [agg_res["severe_hemorrhage"]],
-                        "alertness_motor": [agg_res["alertness_motor"]],
-                        "alertness_verbal": [agg_res["alertness_verbal"]],
+                        "trauma_head": [msg.trauma_head],
+                        "trauma_torso": [msg.trauma_torso],
+                        "trauma_lower_ext": [msg.trauma_lower_ext],
+                        "trauma_upper_ext": [msg.trauma_upper_ext],
+                        "alertness_ocular": [msg.alertness_ocular],
+                        "severe_hemorrhage": [msg.severe_hemorrhage],
+                        "alertness_motor": [msg.alertness_motor],
+                        "alertness_verbal": [msg.alertness_verbal],
                     }
                 )
                 database = database.append(new_row, ignore_index=True)
@@ -682,21 +682,21 @@ class ScorecardSenderNode:
                 for idx, row in database.iterrows():
                     if row["casualty_id"] == casualty_id:
                         if pd.isna(row["trauma_head"]):
-                            database.loc[idx, "trauma_head"] = agg_res["trauma_head"]
+                            database.loc[idx, "trauma_head"] = msg.trauma_head
                         if pd.isna(row["trauma_torso"]):
-                            database.loc[idx, "trauma_torso"] = agg_res["trauma_torso"]
+                            database.loc[idx, "trauma_torso"] = msg.trauma_torso
                         if pd.isna(row["trauma_lower_ext"]):
-                            database.loc[idx, "trauma_lower_ext"] = agg_res["trauma_lower_ext"]
+                            database.loc[idx, "trauma_lower_ext"] = msg.trauma_lower_ext
                         if pd.isna(row["trauma_upper_ext"]):
-                            database.loc[idx, "trauma_upper_ext"] = agg_res["trauma_upper_ext"]
+                            database.loc[idx, "trauma_upper_ext"] = msg.trauma_upper_ext
                         if pd.isna(row["alertness_ocular"]):
-                            database.loc[idx, "alertness_ocular"] = agg_res["alertness_ocular"]
+                            database.loc[idx, "alertness_ocular"] = msg.alertness_ocular
                         if pd.isna(row["severe_hemorrhage"]):
-                            database.loc[idx, "severe_hemorrhage"] = agg_res["severe_hemorrhage"]
+                            database.loc[idx, "severe_hemorrhage"] = msg.severe_hemorrhage
                         if pd.isna(row["alertness_motor"]):
-                            database.loc[idx, "alertness_motor"] = agg_res["alertness_motor"]
+                            database.loc[idx, "alertness_motor"] = msg.alertness_motor
                         if pd.isna(row["alertness_verbal"]):
-                            database.loc[idx, "alertness_verbal"] = agg_res["alertness_verbal"]
+                            database.loc[idx, "alertness_verbal"] = msg.alertness_verbal
                         break
 
                     # if we make it to the end and all entries are full, we add a new row
@@ -704,14 +704,14 @@ class ScorecardSenderNode:
                         new_row = pd.DataFrame(
                             {
                                 "casualty_id": [casualty_id],
-                                "trauma_head": [agg_res["trauma_head"]],
-                                "trauma_torso": [agg_res["trauma_torso"]],
-                                "trauma_lower_ext": [agg_res["trauma_lower_ext"]],
-                                "trauma_upper_ext": [agg_res["trauma_upper_ext"]],
-                                "alertness_ocular": [agg_res["alertness_ocular"]],
-                                "severe_hemorrhage": [agg_res["severe_hemorrhage"]],
-                                "alertness_motor": [agg_res["alertness_motor"]],
-                                "alertness_verbal": [agg_res["alertness_verbal"]],
+                                "trauma_head": [msg.trauma_head],
+                                "trauma_torso": [msg.trauma_torso],
+                                "trauma_lower_ext": [msg.trauma_lower_ext],
+                                "trauma_upper_ext": [msg.trauma_upper_ext],
+                                "alertness_ocular": [msg.alertness_ocular],
+                                "severe_hemorrhage": [msg.severe_hemorrhage],
+                                "alertness_motor": [msg.alertness_motor],
+                                "alertness_verbal": [msg.alertness_verbal],
                             }
                         )
                         database = database.append(new_row, ignore_index=True)
